104936debd7cda9d8154b6bac33562458cdec5b1
Luke Menton <lukementon15@gmail.com>
2022-03-22 22:51:22 +0000

    package json backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,6 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
+const products = require('./data/products');
+import { Product } from './types/types';
 
 const app: Application = express();
 
@@ -6,4 +8,15 @@ app.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.send('Hello');
 });
 
-app.listen(4000, () => console.log('server running'));
+app.get('/products', (req: Request, res: Response, next: NextFunction) => {
+  res.json(products);
+});
+
+app.get('/products/:id', (req: Request, res: Response, next: NextFunction) => {
+  const product = products.find(
+    (product: Product) => product.id === req.params.id
+  );
+  res.json(product);
+});
+
+app.listen(5000, () => console.log('server running'));
