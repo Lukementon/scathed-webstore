27fe0992ef895267da79ac6857ecd93b34dff877
Luke Menton <lukementon15@gmail.com>
2022-03-22 23:23:20 +0000

    progress

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,19 +4,24 @@ import { Product } from './types/types';
 
 const app: Application = express();
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
+app.get('/api', (req: Request, res: Response, next: NextFunction) => {
   res.send('Hello');
 });
 
-app.get('/products', (req: Request, res: Response, next: NextFunction) => {
+app.get('/api/products', (req: Request, res: Response, next: NextFunction) => {
   res.json(products);
 });
 
-app.get('/products/:id', (req: Request, res: Response, next: NextFunction) => {
-  const product = products.find(
-    (product: Product) => product.id === req.params.id
-  );
-  res.json(product);
-});
+app.get(
+  '/api/products/:id',
+  (req: Request, res: Response, next: NextFunction) => {
+    const product = products.find(
+      (product: Product) => product.id === req.params.id
+    );
+
+    console.log('product', product);
+    res.json(product);
+  }
+);
 
 app.listen(5000, () => console.log('server running'));
