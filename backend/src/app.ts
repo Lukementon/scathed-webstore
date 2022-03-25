bbb8b57e79d60fcb52e5fbbf45c3824e3c586d84
Luke <lukem@therapieclinic.com>
2022-03-25 20:50:43 +0000

    backend error handling middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express, { Application } from 'express';
+import { notFound, errorHandler } from './middleware/errorMiddleware';
 import connectDB from './config/db';
 import productRoutes from './routes/productRoutes';
 
@@ -10,6 +11,9 @@ connectDB();
 const app: Application = express();
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
